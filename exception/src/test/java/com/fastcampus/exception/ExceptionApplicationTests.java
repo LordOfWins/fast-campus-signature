c26ca3859a44f161019baf5c507b61583134f398
윤승재 <sjyun011278@gmail.com>
2024-04-07 00:52:48 +0900

    예외 처리 프로젝트

---
+++ exception/src/test/java/com/fastcampus/exception/ExceptionApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fastcampus.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
