9b8e947c43ab06e87119ec3160fffeb9754d703d
윤승재 <sjyun011278@gmail.com>
2024-04-11 08:39:00 +0900

    initial project

---
+++ memorydb/src/test/java/org/web/memorydb/MemorydbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.web.memorydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorydbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
