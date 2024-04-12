f01ee40499288ce5140685a6bb843bf440ad1ca1
윤승재 <sjyun011278@gmail.com>
2024-04-12 21:53:00 +0900

    DataRepository

--- memorydb/src/main/java/org/web/memorydb/db/DataRepository.java
+++ memorydb/src/main/java/org/web/memorydb/db/DataRepository.java
@@ -3,17 +3,20 @@ package org.web.memorydb.db;
 import java.util.List;
 import java.util.Optional;
 
-public interface DataRepository<T, ID> extends Repository<T, ID> {
+public interface DataRepository<T, I> extends Repository<T, I> {
 
   //create
   T save(T data);
 
   //read
-  Optional<T> findById(ID id);
+  Optional<T> findById(I id);
 
   List<T> findAll();
 
+  //update
+  T update(T data);
+
   //delete
-  void deleteById(ID id);
+  void deleteById(I id);
 
 }
