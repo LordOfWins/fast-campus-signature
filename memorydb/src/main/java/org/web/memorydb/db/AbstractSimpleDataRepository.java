b5037634579464b7834be4cbf1b5cff1026eef46
윤승재 <sjyun011278@gmail.com>
2024-04-12 21:53:00 +0900

    AbstractSimpleDataRepository

--- memorydb/src/main/java/org/web/memorydb/db/AbstractSimpleDataRepository.java
+++ memorydb/src/main/java/org/web/memorydb/db/AbstractSimpleDataRepository.java
@@ -1,23 +1,16 @@
 package org.web.memorydb.db;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
-import org.web.memorydb.entitiy.Entity;
+import org.web.memorydb.entitiy.AbstractEntity;
 
-public abstract class AbstractSimpleDataRepository<T extends Entity, I> implements DataRepository<T, I> {
+public abstract class AbstractSimpleDataRepository<T extends AbstractEntity, I> implements DataRepository<T, I> {
 
   private final Map<Long, T> dataMap = new HashMap<>();
-  private final Comparator<I> comparator = new Comparator<I>() {
-    @Override
-    public int compare(I o1, I o2) {
-      return Long.compare((Long)o1, (Long)o2);
-    }
-  };
   private long index;
 
   //create
@@ -27,17 +20,8 @@ public abstract class AbstractSimpleDataRepository<T extends Entity, I> implemen
     if (Objects.isNull(data)) {
       throw new IllegalArgumentException("Data cannot be null");
     }
-    //db에 데이터가 있는가?
-    var prevData = dataMap.get(data.getId());
-
-    if (prevData != null) {
-      //db에 데이터가 있으면 수정
-      dataMap.put(data.getId(), data);
-    } else {
-      //db에 데이터가 없으면 추가
-      data.setId(index++);
-      dataMap.put(data.getId(), data);
-    }
+    data.setId(++index);
+    dataMap.put(data.getId(), data);
     return data;
   }
 
@@ -50,7 +34,23 @@ public abstract class AbstractSimpleDataRepository<T extends Entity, I> implemen
 
   @Override
   public List<T> findAll() {
-    return dataMap.values().stream().sorted(Comparator.comparing(T::getId)).toList();
+    return List.copyOf(dataMap.values());
+  }
+
+  //update
+  @Override
+  public T update(T data) {
+    if (Objects.isNull(data)) {
+      throw new IllegalArgumentException("Data cannot be null");
+    }
+    var prevData = dataMap.values().stream().filter(d -> d.getId().equals(data.getId())).findFirst();
+
+    if (prevData.isPresent()) {
+      dataMap.put(data.getId(), data);
+    } else {
+      throw new IllegalArgumentException("Data is not exist");
+    }
+    return data;
   }
 
   //delete
@@ -58,9 +58,10 @@ public abstract class AbstractSimpleDataRepository<T extends Entity, I> implemen
   @Override
   public void deleteById(I i) {
     var deleteEntity = dataMap.values().stream().filter(data -> data.getId().equals(i)).findFirst();
-
     if (deleteEntity.isPresent()) {
       dataMap.remove(deleteEntity.get().getId());
+    } else {
+      throw new IllegalArgumentException("Data is not exist");
     }
   }
 }
