cbb8a4a1790d7289051d674e0520b5a7adc4ab44
윤승재 <sjyun011278@gmail.com>
2024-04-17 20:14:58 +0900

    BoardDto

---
+++ simple-board/src/main/java/org/web/simpleboard/board/model/BoardDto.java
@@ -0,0 +1,28 @@
+package org.web.simpleboard.board.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.web.simpleboard.post.model.PostDto;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BoardDto {
+  private List<PostDto> posts = List.of();
+  private Long id;
+  private String boardName;
+  private String status;
+
+}
