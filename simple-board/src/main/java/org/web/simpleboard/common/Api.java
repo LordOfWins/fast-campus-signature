01188b73fef9f6afbd9712951e7e7087adacbc7d
윤승재 <sjyun011278@gmail.com>
2024-04-17 20:23:13 +0900

    Api

---
+++ simple-board/src/main/java/org/web/simpleboard/common/Api.java
@@ -0,0 +1,19 @@
+package org.web.simpleboard.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Api<T> {
+  private T body;
+  private Pagination pagination;
+}
